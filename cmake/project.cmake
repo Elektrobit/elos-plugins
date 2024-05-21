7990c89926c8c6cc27d667579cbf4e9dbb6f513b
Friedrich Schwedler <fschwedler@emlix.com>
2024-05-21 14:50:00 +0200

    add dummy client plugin source to be build
    Signed-off-by: Friedrich Schwedler <fschwedler@emlix.com>

---
+++ cmake/project.cmake
@@ -0,0 +1,45 @@
+# SPDX-License-Identifier: MIT
+
+macro(project_set_environment)
+  set(CMAKE_C_STANDARD 17)
+  set(CMAKE_C_STANDARD_REQUIRED ON)
+  set(CMAKE_C_EXTENSIONS OFF)
+  set(CMAKE_C_FLAGS_DEBUG "")
+  set(CMAKE_C_FLAGS_RELEASE "")
+  set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+  include(GNUInstallDirs)
+
+  option(ENABLE_ANALYZER "Build with -fanalyzer" ON)
+  option(ENABLE_ASAN "Link with ASAN" ON)
+
+  add_compile_options(
+    -Wshadow -Wall -Wextra -pedantic -D_DEFAULT_SOURCE
+  )
+
+  if(NOT CMAKE_BUILD_TYPE)
+    set(CMAKE_BUILD_TYPE "Debug")
+  endif()
+
+  if(CMAKE_BUILD_TYPE STREQUAL "Debug")
+    if (ENABLE_ASAN)
+      link_libraries(asan)
+      add_compile_options(-fsanitize=address)
+    endif()
+    add_compile_options(
+      -Og -g3 -DDEBUG -fno-omit-frame-pointer
+      $<IF:$<BOOL:${ENABLE_ANALYZER}>,-fanalyzer,>
+      $<IF:$<BOOL:${ENABLE_ANALYZER}>,-Wno-analyzer-malloc-leak,>
+    )
+  elseif(CMAKE_BUILD_TYPE STREQUAL "Release")
+    add_compile_options(-O3 -DNDEBUG -g -Werror)
+  elseif(CMAKE_BUILD_TYPE STREQUAL "Profiling")
+    add_compile_options(-O3 -DNDEBUG -g -fno-omit-frame-pointer)
+  elseif(CMAKE_BUILD_TYPE STREQUAL "Tsan")
+    link_libraries(tsan)
+    add_compile_options(
+      -Og -g3 -DDEBUG -fsanitize=thread -fno-omit-frame-pointer
+      $<IF:$<BOOL:${ENABLE_ANALYZER}>,-fanalyzer,>
+      $<IF:$<BOOL:${ENABLE_ANALYZER}>,-Wno-analyzer-malloc-leak,>
+    )
+  endif()
+endmacro()
